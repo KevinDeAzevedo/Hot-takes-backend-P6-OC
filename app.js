ed0971aa2d96967d0eb86220227ac30dbf056741
KevinDeAzevedo <deazevedo.kevin@gmail.com>
2023-01-24 18:26:46 +0100

    config Mongoose connexion

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const cors = require('cors')
 const app = express()
 
@@ -15,5 +16,16 @@ next()
 app.use((req, res) => {
 	console.log('Requete bien envoyée !')
 })
+
+// Connexion à la Base de donnée MongoDB
+try {
+	mongoose.connect(
+	'mongodb+srv://`${process.env.MONGO_USER}`:`${process.env.MONGO_PASSWORD}`@cluster0.xnhlqf7.mongodb.net/?retryWrites=true&w=majority',
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	() => console.log("Mongoose est connecté")
+);
+} catch (e) {
+console.log("Mongoose n'arrive pas à se connecter");
+}
 
 module.exports = app
